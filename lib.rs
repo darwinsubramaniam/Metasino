d0cc84feb0e878a8e6fab1d0909b7c5c14f93bf2
Darwin Subramaniam <darwinsubramaniam@googlemail.com>
2022-05-18 22:20:56 +0800

    changed the state from being u8 to Enum reference.

--- lib.rs
+++ lib.rs
@@ -12,6 +12,27 @@ mod metasino {
     /// The minimum player required to start the game.
     const MIN_PLAYERS: u8 = 3;
 
+    #[derive(
+        Debug,
+        Copy,
+        Clone,
+        PartialEq,
+        Eq,
+        scale::Encode,
+        scale::Decode,
+        ink_storage::traits::SpreadLayout,
+        ink_storage::traits::PackedLayout,
+    )]
+    #[cfg_attr(
+        feature = "std",
+        derive(::scale_info::TypeInfo, ::ink_storage::traits::StorageLayout)
+    )]
+    pub enum STATE {
+        STAGING,
+        PLAYING,
+        ENDED
+    }
+
     #[ink(event)]
     pub struct NewTableOpened {
         #[ink(topic)]
@@ -47,7 +68,7 @@ mod metasino {
         /// 0: Not started
         /// 1: Started
         /// 3: Ended
-        state: u8,
+        state: STATE,
     }
 
     impl Metasino {
@@ -68,15 +89,13 @@ mod metasino {
                 required_start_bet,
                 players,
                 pot: required_start_bet,
-                state: 0,
+                state: STATE::STAGING,
             }
         }
 
         #[ink(message)]
         pub fn terminate(&mut self) {
-            if self.state == 1 {
-                panic!("Game is ongoing, cannot terminate");
-            }
+            self.table_status_guard();
             if self.get_players().contains(&Self::env().caller()) {
                 panic!("Only the initializer can terminate the game");
             }
@@ -89,9 +108,7 @@ mod metasino {
         /// error if new player places bet less or more than the required start bet.
         #[ink(message)]
         pub fn register_player(&mut self, start_bet: Balance) {
-            if self.state != 0 {
-                panic!("Table is already started/ended");
-            }
+            self.table_status_guard();
             let caller = Self::env().caller();
             if self.get_players_count() >= MAX_PLAYERS {
                 panic!("Max players reached");
@@ -112,19 +129,30 @@ mod metasino {
             }
         }
 
+        /// Start the game by extending the table to the game contract.
         #[ink(message)]
         pub fn start_game(&mut self) {
-            if self.state != 0 {
-                panic!("Table is already started/ended");
-            }
+            self.table_status_guard();
+
             if self.get_players_count() < MIN_PLAYERS {
                 panic!("Minimum {} players required to start the game", MIN_PLAYERS);
             }
-            self.state = 1;
+            self.state = STATE::PLAYING;
+        }
+
+        /// Guarding the contract from being executed in a wrong state.
+        fn table_status_guard(&self){
+            if self.state == STATE::PLAYING {
+                panic!("Game is ongoing!!");
+            }
+
+            if self.state == STATE::ENDED {
+                panic!("Game has already has ended");
+            }
         }
 
         #[ink(message)]
-        pub fn get_table_state(&self) -> u8 {
+        pub fn get_table_state(&self) -> STATE {
             self.state
         }
 
@@ -179,7 +207,7 @@ mod metasino {
             assert_eq!(accounts.alice, metasino.initializer);
             assert_eq!(metasino.get_players_count(), 1);
             assert_eq!(metasino.get_accumulated_pot(), 100);
-            assert_eq!(metasino.get_table_state(), 0);
+            assert_eq!(metasino.get_table_state(), STATE::STAGING);
         }
 
         #[ink::test]
@@ -202,7 +230,7 @@ mod metasino {
             assert_eq!(metasino.get_accumulated_pot(), 200);
             assert_eq!(metasino.get_players()[0], accounts.alice);
             assert_eq!(metasino.get_players()[1], accounts.bob);
-            assert_eq!(metasino.get_table_state(), 0);
+            assert_eq!(metasino.get_table_state(), STATE::STAGING);
             assert_eq!(metasino.get_required_start_bet(), 100);
         }
 
@@ -233,11 +261,11 @@ mod metasino {
             ink_env::test::set_caller::<ink_env::DefaultEnvironment>(accounts.charlie);
             metasino.register_player(100);
             metasino.start_game();
-            assert_eq!(metasino.get_table_state(), 1);
+            assert_eq!(metasino.get_table_state(), STATE::PLAYING);
         }
 
         #[ink::test]
-        #[should_panic = "Table is already started/ended"]
+        #[should_panic = "Game is ongoing!!"]
         fn fail_to_add_player_when_game_status_started(){
             let accounts = ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
             ink_env::test::set_caller::<ink_env::DefaultEnvironment>(accounts.alice);
@@ -253,7 +281,7 @@ mod metasino {
         }
 
         #[ink::test]
-        #[should_panic = "Game is ongoing, cannot terminate"]
+        #[should_panic = "Game is ongoing!!"]
         fn should_not_allow_termination_if_table_game_in_started_state(){
             let accounts = ink_env::test::default_accounts::<ink_env::DefaultEnvironment>();
             ink_env::test::set_caller::<ink_env::DefaultEnvironment>(accounts.alice);
